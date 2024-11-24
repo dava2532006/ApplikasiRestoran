87567d401b8b89bb3bc20dad043a19886ab46612
dava12321 <55383271+dava12321@users.noreply.github.com>
2024-11-24 20:10:27 +0700

    koneksi ke db nya guis

--- src/appliaski_restoran/Appliaski_Restoran.java
+++ src/appliaski_restoran/Appliaski_Restoran.java
@@ -14,7 +14,9 @@ public class Appliaski_Restoran {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       koneksi k = new koneksi();
+       k.connect();
+      
     }
     
 }
